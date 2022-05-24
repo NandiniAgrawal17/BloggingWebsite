4f6a4a7892ab08e0f0abdf5035af557cb7ccc75f
NandiniAgrawal17 <nandini.agrawal_cs19@gla.ac.in>
2022-05-24 13:52:05 +0530

    created settings page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Topbar from "./components/topbar/Topbar";
 import Home from "./components/pages/home/Home";
 import Single from "./components/pages/single/Single";
 import Write from "./components/pages/write/Write";
+import Settings from "./components/pages/settings/Setting";
 function App() {
   return (
     <>
     <Topbar/>
-  <Write/> 
+  <Settings/> 
   </>
   
   );
