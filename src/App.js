fd01ac3acec87bdc34047374ec2614dc11bbd4ce
NandiniAgrawal17 <nandini.agrawal_cs19@gla.ac.in>
2022-05-24 12:57:19 +0530

    created components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
-
+import Topbar from "./topbar/Topbar";
+import Home from "./pages/home/Home";
 function App() {
   return (
-    <div className="App">
-      blog app
-    </div>
+    <>
+    <Topbar/>
+  <Home/>
+  </>
+  
   );
 }
 
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,19 @@
+//import { useLocation } from "react-router";
+import Header from "../../header/Header";
+import Posts from "../../posts/Posts";
+import Sidebar from "../../sidebar/Sidebar";
+import "./home.css";
+
+export default function Homepage() {
+  //const location = useLocation();
+ // console.log(location);
+  return (
+    <>
+      <Header />
+      <div className="home">
+        <Posts />
+        <Sidebar />
+      </div>
+    </>
+  );
+}
